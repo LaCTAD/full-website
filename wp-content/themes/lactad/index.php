23719facf3f4766fca1f52d4952c00805321b136
dsfugimoto <dsfugimoto@gmail.com>
2015-02-10 11:07:58 -0200

    First commit

---
+++ wp-content/themes/lactad/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>LaCTAD - Laboratório Central de Tecnologias de Alto Desempenho em Ciências da Vida</title>
+	</head>
+	<body>
+		teste123
+	</body>
+</html>
