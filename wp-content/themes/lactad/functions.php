20c06c5c76da5d7bbbfbb6ecec8017d2a9699188
Felipe Bidu <bidu.pub@gmail.com>
2015-08-11 11:52:16 -0300

    Done! Analytics added

--- wp-content/themes/lactad/functions.php
+++ wp-content/themes/lactad/functions.php
@@ -445,13 +445,13 @@ add_filter( 'json_prepare_post', 'json_api_prepare_post', 10, 3 );
 
 // Inserting the Analytics Tracker
 add_action('wp_footer', 'add_google_analytics');
-function add_google_analytics() { 
-  "<script>
+function add_google_analytics() {  ?>
+<script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function() {
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
   ga('create', 'UA-51070619-1', 'auto');
   ga('send', 'pageview');
-  </script>"
-}
+  </script>
+<?php } ?>
