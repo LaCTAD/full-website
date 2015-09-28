447b39e890787b5b3d753d2070993c12ca2fb3a2
Felipe Bidu <bidu.pub@gmail.com>
2015-09-28 16:58:03 -0300

    Adding anti abuse headers

--- wp-content/themes/lactad/send.php
+++ wp-content/themes/lactad/send.php
@@ -9,7 +9,16 @@
 	$email->Body      = "olar";
 	$email->AddAddress( 'dsfugimoto@gmail.com' );
 	$email->AddAttachment( $_POST['file'] );
-
+	$email->HeaderLine("Organization" , "LaCTAD"); 
+	$email->HeaderLine("Content-Transfer-encoding" , "8bit");
+	$email->HeaderLine("Message-ID" , "<".md5(uniqid(time()))."@{$_SERVER['SERVER_NAME']}>");
+	$email->HeaderLine("X-MSmail-Priority" , "Normal");
+	$email->HeaderLine("X-Mailer" , "Microsoft Office Outlook, Build 11.0.5510");
+	$email->HeaderLine("X-MimeOLE" , "Produced By Microsoft MimeOLE V6.00.2800.1441");
+	$email->HeaderLine("X-Sender" , $this->Sender);
+	$email->HeaderLine("X-AntiAbuse" , "This is a solicited email for - LaCTAD");
+	$email->HeaderLine("X-AntiAbuse" , "Servername - {$_SERVER['SERVER_NAME']}");
+	$email->HeaderLine("X-AntiAbuse" , $this->Sender);
 	$send = $email->Send();
 
 	$email->ClearAllRecipients();
