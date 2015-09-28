a5b07dfcc7492461f515173d47690a103c14d02c
Felipe Bidu <bidu.pub@gmail.com>
2015-09-28 17:04:37 -0300

    Trying to fix the headers

--- wp-content/themes/lactad/send.php
+++ wp-content/themes/lactad/send.php
@@ -11,14 +11,11 @@
 	$email->AddAttachment( $_POST['file'] );
 	$email->HeaderLine("Organization" , "LaCTAD"); 
 	$email->HeaderLine("Content-Transfer-encoding" , "8bit");
-	$email->HeaderLine("Message-ID" , "<" + md5(uniqid(time())) + "$_SERVER['SERVER_NAME']>");
 	$email->HeaderLine("X-MSmail-Priority" , "Normal");
 	$email->HeaderLine("X-Mailer" , "Microsoft Office Outlook, Build 11.0.5510");
 	$email->HeaderLine("X-MimeOLE" , "Produced By Microsoft MimeOLE V6.00.2800.1441");
-	$email->HeaderLine("X-Sender" , $email->Sender);
 	$email->HeaderLine("X-AntiAbuse" , "This is a solicited email for - LaCTAD");
-	$email->HeaderLine("X-AntiAbuse" , "Servername - {$_SERVER['SERVER_NAME']}");
-	$email->HeaderLine("X-AntiAbuse" , $email->Sender);
+	$email->HeaderLine("X-AntiAbuse" , "Servername - Lactads01");
 	$send = $email->Send();
 
 	$email->ClearAllRecipients();
