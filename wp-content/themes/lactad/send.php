8215e2b7d5273335de15a5ea00d31927388098a0
Felipe Bidu <bidu.pub@gmail.com>
2015-09-28 17:15:20 -0300

    Dirty fix

--- wp-content/themes/lactad/send.php
+++ wp-content/themes/lactad/send.php
@@ -31,4 +31,22 @@
 		echo "Não foi possível enviar o e-mail.";
 		echo "<b>Informações do erro:</b> " . $email->ErrorInfo;
 	}
+	<table>
+ 
+
+
+    foreach ($_POST as $key => $value) {
+        echo "<tr>";
+        echo "<td>";
+        echo $key;
+        echo "</td>";
+        echo "<td>";
+        echo $value;
+        echo "</td>";
+        echo "</tr>";
+    }
+
+
+
+</table>
 ?>
