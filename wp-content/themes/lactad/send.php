c355c5689fc5dc7583e8266ea1bebc2f3a78e696
Felipe Bidu <bidu.pub@gmail.com>
2015-09-28 16:49:29 -0300

    Removing the conditional from send.php

--- wp-content/themes/lactad/send.php
+++ wp-content/themes/lactad/send.php
@@ -1,27 +1,28 @@
 <?php
-require_once('assets/php/PHPMailer/PHPMailerAutoload.php');
+	require_once('assets/php/PHPMailer/PHPMailerAutoload.php');
 
-if($_POST['type'] == 'contact') {
-    $email = new PHPMailer(True);
+    $email = new PHPMailer;
     $email->IsSendmail();
-  $email->From      = $_POST['email'];
-  $email->FromName  = $_POST['name'];
-  $email->Subject   = $_POST['subject'];
-  $email->Body      = $_POST['message'];
-  $email->AddAddress( 'dsfugimoto@gmail.com' );
-  $email->AddAttachment( $_POST['file'] );
+	$email->From      = $_POST['email'];
+	$email->FromName  = $_POST['name'];
+	$email->Subject   = $_POST['subject'];
+	$email->Body      = $_POST['message'];
+	$email->AddAddress( 'dsfugimoto@gmail.com' );
+	$email->AddAttachment( $_POST['file'] );
 
-  $send = $email->Send();
+	$send = $email->Send();
 
-  $email->ClearAllRecipients();
-  $email->ClearAttachments();
+	$email->ClearAllRecipients();
+	$email->ClearAttachments();
+
   // Exibe uma mensagem de resultado
-  if ($send) {
-    echo "E-mail enviado com sucesso!";
-  } else {
-    echo "Não foi possível enviar o e-mail.";
-    echo "<b>Informações do erro:</b> " . $email->ErrorInfo;
-  }
-}
-
+	if ($send)
+	{
+		echo "E-mail enviado com sucesso!";
+	}
+	else
+	{
+		echo "Não foi possível enviar o e-mail.";
+		echo "<b>Informações do erro:</b> " . $email->ErrorInfo;
+	}
 ?>
