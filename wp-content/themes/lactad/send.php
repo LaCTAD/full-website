494185a2a0b00f17849822401a8ea5b425a090f3
Felipe Bidu <bidu.pub@gmail.com>
2015-10-13 15:57:39 -0300

    Adding Sandra's email

--- wp-content/themes/lactad/send.php
+++ wp-content/themes/lactad/send.php
@@ -11,7 +11,8 @@
 		$email->Body      = $_POST['message'];
 		$email->AddAddress('dsfugimoto@gmail.com');
 		$email->AddAddress('bidu.pub@gmail.com');
-		$email->AddAddress('adm.lactad@reitoria.unicamp.br');
+        $email->AddAddress('adm.lactad@reitoria.unicamp.br');
+        $email->AddAddress('sandra.krauchenco@reitoria.unicamp.br');
 		$email->AddReplyTo($_POST['email'], $_POST['name']);
 		$email->AddAttachment( $_POST['file'] );
 		$email->HeaderLine("Organization" , "LaCTAD");
