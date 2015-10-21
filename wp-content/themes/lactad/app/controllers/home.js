d47c0dbe124c27292a06a44173c549e7a4d643c0
dsfugimoto <dsfugimoto@gmail.com>
2015-10-21 10:38:20 -0200

    Feat: adding top notifications for schedules changes

--- wp-content/themes/lactad/app/controllers/home.js
+++ wp-content/themes/lactad/app/controllers/home.js
@@ -1,14 +1,15 @@
 app.controller('HomeController', [
   '$scope',
   '$timeout',
-  function($scope, posts, events, $timeout) {
+  '$posts',
+  function($scope, $timeout, $posts) {
     console.log("rota home");
     var locked = 0;
 
     jQuery(window).scroll(function() {
       if (jQuery(this).scrollTop() < 700 && locked == 0) {
         jQuery('#tera').stop().animateNumber({
-          number: 10.91,
+          number: 12.83,
           numberStep: function(now, tween) {
             jQuery(tween.elem).text(now.toFixed(2));
           }
@@ -27,4 +28,11 @@ app.controller('HomeController', [
         // locked = 0;
       }
     });
+
+    // get mudancas-de-horario posts
+    $posts.category('post', 'mudanca-de-horario').then(function(data) {
+      $scope.mudancas = data;
+      console.log(data);
+    });
+
 }]);
