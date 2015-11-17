1bb68c74a255a27d299db7737de967820d22d4b1
Felipe Bidu <bidu.pub@gmail.com>
2015-11-17 14:04:51 -0200

    Atualizando o numero de pesquisadores

--- wp-content/themes/lactad/app/controllers/home.js
+++ wp-content/themes/lactad/app/controllers/home.js
@@ -17,7 +17,7 @@ app.controller('HomeController', [
           number: 14
         }, 2500);
         jQuery("#research").stop().animateNumber({
-          number: 350
+          number: 359
         }, 2500);
         jQuery("#courses").stop().animateNumber({
           number: 15
