352750d7dcaa273a7bba61828fc822f06cc34ac8
dsfugimoto <dsfugimoto@gmail.com>
2015-12-02 10:35:58 -0200

    Fix: getting content on event modal

--- wp-content/themes/lactad/app/controllers/modal.js
+++ wp-content/themes/lactad/app/controllers/modal.js
@@ -1,7 +1,9 @@
-app.controller('ModalController', function($scope, close, $sce) {
+app.controller('ModalController', function($rootScope, $scope, close, $sce) {
   $scope.close = function(result) {
     close(result, 500); // close, but give 500ms for bootstrap to animate
   };
 
   $scope.$sce = $sce;
+
+  console.log($rootScope.pages.events.subpages.registered.name);
 });
