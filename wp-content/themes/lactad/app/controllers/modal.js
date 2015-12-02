751f09e9f61bb0e3460524616394d31446dea5b8
dsfugimoto <dsfugimoto@gmail.com>
2015-12-02 10:55:31 -0200

    Debugging

--- wp-content/themes/lactad/app/controllers/modal.js
+++ wp-content/themes/lactad/app/controllers/modal.js
@@ -3,5 +3,6 @@ app.controller('ModalController', function($rootScope, $scope, close, $sce) {
     close(result, 500); // close, but give 500ms for bootstrap to animate
   };
   $scope.content = $rootScope.event.register_form;
+  console.log($scope.content);
   $scope.$sce = $sce;
 });
