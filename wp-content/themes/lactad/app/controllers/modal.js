e38a8146e10d69198b58470fcae4d49288d10e86
dsfugimoto <dsfugimoto@gmail.com>
2015-12-02 11:02:58 -0200

    Debugging event

--- wp-content/themes/lactad/app/controllers/modal.js
+++ wp-content/themes/lactad/app/controllers/modal.js
@@ -2,7 +2,7 @@ app.controller('ModalController', function($rootScope, $scope, close, $sce) {
   $scope.close = function(result) {
     close(result, 500); // close, but give 500ms for bootstrap to animate
   };
-  $scope.content = $rootScope.event.register_form;
+  $scope.content = $rootScope.event.event.register_form;
   console.log($scope.content);
   $scope.$sce = $sce;
 });
