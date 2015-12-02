6237cd33252c162d7a8a17b1007535cc8f2f4aec
dsfugimoto <dsfugimoto@gmail.com>
2015-12-02 10:29:35 -0200

    Fix:  on modal

--- wp-content/themes/lactad/app/controllers/modal.js
+++ wp-content/themes/lactad/app/controllers/modal.js
@@ -1,5 +1,7 @@
-app.controller('ModalController', function($scope, close) {
+app.controller('ModalController', function($scope, close, $sce) {
   $scope.close = function(result) {
     close(result, 500); // close, but give 500ms for bootstrap to animate
   };
+
+  $scope.$sce = $sce;
 });
