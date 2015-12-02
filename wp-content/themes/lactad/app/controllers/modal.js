e1157f13809855418e80e92114855a89bbd9b2df
dsfugimoto <dsfugimoto@gmail.com>
2015-12-02 11:12:55 -0200

    Fix: event modal now displaying proper content

--- wp-content/themes/lactad/app/controllers/modal.js
+++ wp-content/themes/lactad/app/controllers/modal.js
@@ -3,6 +3,5 @@ app.controller('ModalController', function($rootScope, $scope, close, $sce) {
     close(result, 500); // close, but give 500ms for bootstrap to animate
   };
   $scope.content = $rootScope.event.event.register_form;
-  console.log($scope.content);
   $scope.$sce = $sce;
 });
