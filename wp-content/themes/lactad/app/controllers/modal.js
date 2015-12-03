b6b2853f72203a02b6eee24c4ecd2c0a473e07af
dsfugimoto <dsfugimoto@gmail.com>
2015-12-03 09:08:29 -0200

    Fix: modals

--- wp-content/themes/lactad/app/controllers/modal.js
+++ wp-content/themes/lactad/app/controllers/modal.js
@@ -2,7 +2,8 @@ app.controller('ModalController', function($rootScope, $scope, close, $sce) {
   $scope.close = function(result) {
     close(result, 500); // close, but give 500ms for bootstrap to animate
   };
-  console.log($rootScope.page);
-  $scope.content = $rootScope.event.event.register_form;
+  if($rootScope.page.id == 'events') {
+    $scope.content = $rootScope.event.event.register_form;
+  }
   $scope.$sce = $sce;
 });
