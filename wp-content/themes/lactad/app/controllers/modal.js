551c5e04b032dfeb7e8657ba918f8e332f7cb2ea
dsfugimoto <dsfugimoto@gmail.com>
2015-12-03 09:06:32 -0200

    Fix: modals breaking outside event page

--- wp-content/themes/lactad/app/controllers/modal.js
+++ wp-content/themes/lactad/app/controllers/modal.js
@@ -2,6 +2,7 @@ app.controller('ModalController', function($rootScope, $scope, close, $sce) {
   $scope.close = function(result) {
     close(result, 500); // close, but give 500ms for bootstrap to animate
   };
+  console.log($rootScope.page);
   $scope.content = $rootScope.event.event.register_form;
   $scope.$sce = $sce;
 });
