6c96f9d011035988390374e05d33fd5f88e91e82
dsfugimoto <dsfugimoto@gmail.com>
2016-01-08 14:08:43 -0200

    Feat: event form validation

--- wp-content/themes/lactad/app/controllers/page.js
+++ wp-content/themes/lactad/app/controllers/page.js
@@ -55,9 +55,46 @@ app.controller('PageController', function($scope, $rootScope, $http, $state, $st
     console.log('haha');
     jQuery('body').on('submit', 'form:first', function(event) {
       event.preventDefault();
-      event.stopPropagation();
-      alert('para tuuudo!');
+      var hasFile = jQuery(this).find("input[type='file']").val();
+      var dateCorrect = isDate(jQuery(this).find("input.datepicker").val());
+      if(hasFile && dateCorrect) {
+        jQuery(this).submit();
+      } else if (hasFile == false) {
+        alert('Por favor, anexe um arquivo antes de enviar o formulário!');
+      } else if (dateCorrect == false) {
+        alert('Por favor, insira uma data válida e no formato dd/mm/yyyy!');
+      }
     });
+
+    function isDate(txtDate) {
+      var currVal = txtDate;
+      if(currVal == '')
+        return false;
+
+      var rxDatePattern = /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/; //Declare Regex
+      var dtArray = currVal.match(rxDatePattern); // is format OK?
+
+      if (dtArray == null)
+        return false;
+
+      //Checks for dd/mm/yyyy format.
+      dtDay = dtArray[1];
+      dtMonth = dtArray[3];
+      dtYear = dtArray[5];
+
+      if (dtDay < 1 || dtDay> 31)
+        return false;
+      else if (dtMonth < 1 || dtMonth > 12)
+        return false;
+      else if ((dtMonth == 4 || dtMonth == 6 || dtMonth == 9 || dtMonth == 11) && dtDay == 31)
+        return false;
+      else if (dtMonth == 2) {
+        var isleap = (dtYear % 4 == 0 && (dtYear % 100 != 0 || dtYear % 400 == 0));
+        if (dtDay > 29 || (dtDay == 29 && !isleap))
+          return false;
+      }
+      return true;
+    }
   };
 
   $scope.contact = {};
