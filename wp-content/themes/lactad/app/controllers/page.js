eb966a837edda90955917025b4c6f1d5b9f1d20f
dsfugimoto <dsfugimoto@gmail.com>
2016-01-12 11:57:19 -0200

    Fix empty fields

--- wp-content/themes/lactad/app/controllers/page.js
+++ wp-content/themes/lactad/app/controllers/page.js
@@ -58,6 +58,7 @@ app.controller('PageController', function($scope, $rootScope, $http, $state, $st
       var formFilled = true;
       jQuery(".gform_fields :input").not("input[type='submit'], input[type='hidden']").each(function() {
         if ((jQuery(this).is(":checkbox") && !jQuery(this).is(":checked")) || (jQuery(this).is(":radio") && !jQuery(this).is(":checked")) || jQuery.trim(jQuery(this).val()) === "") {
+          console.log(jQuery(this).attr('id'));
           formFilled = false;
         }
       });
